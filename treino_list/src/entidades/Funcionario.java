c1dd245e94490b8bbf680e3e07485028e5797956
LeoDiasCarvalho <leodiascarvalhosr@gmail.com>
2021-08-01 09:57:08 -0300

    Criando classe Funcionario

---
+++ treino_list/src/entidades/Funcionario.java
@@ -0,0 +1,60 @@
+package entidades;
+
+import java.util.Date;
+
+public class Funcionario {
+	
+	private int id;
+	private String nome;
+	private Date dataNascimento;
+	private double salario;
+	
+	public Funcionario() {
+	}
+
+	public Funcionario(int id, String nome, Date dataNascimento, double salario) {
+		this.id = id;
+		this.nome = nome;
+		this.dataNascimento = dataNascimento;
+		this.salario = salario;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	
+	
+	
+	
+	
+
+}
